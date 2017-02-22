4da3124818688ae1c9715aa8d4d5769fdbced244
多田真敏 <tadamasatoshi@tadamasatoshinoiMac.local>
2017-02-22 15:17:37 +0900

    fix build script

--- build-all.sh
+++ build-all.sh
@@ -5,5 +5,7 @@ mvn -f ./eureka-server clean package -Dmaven.test.skip
 mvn -f ./goods-service clean package -Dmaven.test.skip
 mvn -f ./hystrix-dashboard clean package -Dmaven.test.skip
 mvn -f ./order-service clean package -Dmaven.test.skip
+mvn -f ./customer-api-gateway clean package -Dmaven.test.skip
+mvn -f ./staff-api-gateway clean package -Dmaven.test.skip
 mvn -f ./ui-customer-service clean package -Dmaven.test.skip
 mvn -f ./ui-staff-service clean package -Dmaven.test.skip
